e0cde0d8fcac8da007014007697fb814e6230a29
Rukayat-T <tewogbaderukayat@gmail.com>
2023-06-30 19:40:58 +0100

    sequelize set up

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,14 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
+
+const db = require('./models')
+
+db.sequelize.sync().then((req) => {
 const port = 4000
 app.listen(port, ()=>{
     console.log(`Server started on ${port}.`
     )
 })
+})
+
